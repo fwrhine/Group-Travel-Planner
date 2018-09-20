942621eeac54e692976d67e0b447de170a54b59a
naufalhendry <naufal.rayhan@gmail.com>
2018-09-20 17:02:16 +0700

    Allow query of data from search input

--- app/src/main/java/com/example/pplki18/grouptravelplanner/SearchBarActivity.java
+++ app/src/main/java/com/example/pplki18/grouptravelplanner/SearchBarActivity.java
@@ -2,31 +2,38 @@ package com.example.pplki18.grouptravelplanner;
 
 import android.app.SearchManager;
 import android.content.Intent;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
 import com.example.pplki18.grouptravelplanner.data.DatabaseHelper;
+import com.example.pplki18.grouptravelplanner.data.UserContract;
 
 public class SearchBarActivity extends AppCompatActivity {
 
-    DatabaseHelper dbhelper;
+    DatabaseHelper myDb;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_search_bar);
+        myDb = new DatabaseHelper(this);
 
         // Get the intent, verify the action and get the query
         Intent intent = getIntent();
         if (Intent.ACTION_SEARCH.equals(intent.getAction())) {
             String query = intent.getStringExtra(SearchManager.QUERY);
-            //doMySearch(query);
+            Cursor result = searchData(query);
         }
     }
 
-    public String searchData(String query) {
-        String command = "SELECT * FROM ";
+    public Cursor searchData(String query) {
+        SQLiteDatabase db = myDb.getReadableDatabase();
+        String command = "SELECT * FROM " + UserContract.UserEntry.TABLE_NAME + " WHERE " +
+        UserContract.UserEntry._ID + "=" + query;
+        Cursor data = db.rawQuery(command, null);
 
-        return command;
+        return data;
     }
 }
