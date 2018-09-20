f440ee1308a8278f506c6b6d8a899971065f21c8
naufalhendry <naufal.rayhan@gmail.com>
2018-09-20 18:10:53 +0700

    Allow to check if the user is available or not

--- app/src/main/java/com/example/pplki18/grouptravelplanner/SearchBarActivity.java
+++ app/src/main/java/com/example/pplki18/grouptravelplanner/SearchBarActivity.java
@@ -6,12 +6,16 @@ import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.widget.CursorAdapter;
+import android.widget.Toast;
 
 import com.example.pplki18.grouptravelplanner.data.DatabaseHelper;
 import com.example.pplki18.grouptravelplanner.data.UserContract;
 
 public class SearchBarActivity extends AppCompatActivity {
 
+    CursorAdapter adapter;
     DatabaseHelper myDb;
 
     @Override
@@ -19,19 +23,29 @@ public class SearchBarActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_search_bar);
         myDb = new DatabaseHelper(this);
+        Cursor result;
 
         // Get the intent, verify the action and get the query
         Intent intent = getIntent();
         if (Intent.ACTION_SEARCH.equals(intent.getAction())) {
             String query = intent.getStringExtra(SearchManager.QUERY);
-            Cursor result = searchData(query);
+            result = searchData(query);
+
+            if (result.getCount() == 0) {
+                Toast.makeText(SearchBarActivity.this, "No user found",
+                        Toast.LENGTH_SHORT).show();
+            }
+
+            else {
+
+            }
         }
     }
 
     public Cursor searchData(String query) {
         SQLiteDatabase db = myDb.getReadableDatabase();
         String command = "SELECT * FROM " + UserContract.UserEntry.TABLE_NAME + " WHERE " +
-        UserContract.UserEntry._ID + "=" + query;
+        UserContract.UserEntry.COL_USERNAME + "=" + query;
         Cursor data = db.rawQuery(command, null);
 
         return data;
