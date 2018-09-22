c25d3e91870790b0490146473d859c03830987e9
naufalhendry <naufal.rayhan@gmail.com>
2018-09-22 16:37:27 +0700

    Change query of item

--- app/src/main/java/com/example/pplki18/grouptravelplanner/SearchBarActivity.java
+++ app/src/main/java/com/example/pplki18/grouptravelplanner/SearchBarActivity.java
@@ -1,7 +1,5 @@
 package com.example.pplki18.grouptravelplanner;
 
-import android.app.SearchManager;
-import android.content.Intent;
 import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 import android.support.v7.app.AppCompatActivity;
@@ -9,7 +7,6 @@ import android.os.Bundle;
 import android.util.Log;
 import android.widget.ListView;
 import android.widget.SearchView;
-import android.widget.Toast;
 
 import com.example.pplki18.grouptravelplanner.data.DatabaseHelper;
 import com.example.pplki18.grouptravelplanner.data.UserContract;
@@ -26,38 +23,26 @@ public class SearchBarActivity extends AppCompatActivity {
         setContentView(R.layout.activity_search_bar);
         myDb = new DatabaseHelper(this);
 
-        // Get the intent, verify the action and get the query
-
-        /*Intent intent = getIntent();
-        if (Intent.ACTION_SEARCH.equals(intent.getAction())) {
-            String query = intent.getStringExtra(SearchManager.QUERY);
-            result = searchData(query);
-            */
-
         final SearchView simpleSearchView = (SearchView) findViewById(R.id.search); // inititate a search view
 
         simpleSearchView.setOnQueryTextListener(new SearchView.OnQueryTextListener() {
             @Override
             public boolean onQueryTextSubmit(String query) {
                 // do something on text submit
-                result = searchData(query.toString());
 
-            /*if (result.getCount() == 0) {
-                Log.d("QUERY", "FAIL_SEARCH");
-                Toast.makeText(SearchBarActivity.this, "No user found",
-                        Toast.LENGTH_SHORT).show();
-            } else {*/
-                Log.d("QUERY", "START_SEARCH");
-                ListView searchItems = (ListView) findViewById(R.id.list);
-                SearchCursorAdapter adapter = new SearchCursorAdapter(getApplicationContext(), result);
-                searchItems.setAdapter(adapter);
-                return true;
+                return false;
             }
 
             @Override
             public boolean onQueryTextChange(String newText) {
                 // do something when text changes
-                return false;
+                result = searchData(newText.toString());
+
+                Log.d("QUERY", "START_SEARCH");
+                ListView searchItems = (ListView) findViewById(R.id.list);
+                SearchCursorAdapter adapter = new SearchCursorAdapter(getApplicationContext(), result);
+                searchItems.setAdapter(adapter);
+                return true;
             }
         });
 
@@ -66,8 +51,8 @@ public class SearchBarActivity extends AppCompatActivity {
     public Cursor searchData(String query) {
         SQLiteDatabase db = myDb.getReadableDatabase();
         String command = "SELECT * FROM " + UserContract.UserEntry.TABLE_NAME + " WHERE " +
-                UserContract.UserEntry.COL_USERNAME + "=?";
-        String[] selectionArgs = new String[]{query};
+                UserContract.UserEntry.COL_USERNAME + " LIKE ?";
+        String[] selectionArgs = new String[]{"%" + query + "%"};
         Cursor data = db.rawQuery(command, selectionArgs);
         Log.d("DATA", query);
         Log.d("DATA", data.getCount() + ".");
