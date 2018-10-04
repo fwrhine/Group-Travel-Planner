ee8fb079f816c0cd8a29d2827f273cec2b92a37a
unknown <bayusuto@gmail.com>
2018-10-04 17:46:37 +0700

    add import

--- app/src/main/java/com/example/pplki18/grouptravelplanner/Fragment_Friends.java
+++ app/src/main/java/com/example/pplki18/grouptravelplanner/Fragment_Friends.java
@@ -19,6 +19,7 @@ import android.view.ViewGroup;
 import com.example.pplki18.grouptravelplanner.data.DatabaseHelper;
 import com.example.pplki18.grouptravelplanner.data.FriendsContract;
 import com.example.pplki18.grouptravelplanner.data.UserContract;
+import com.example.pplki18.grouptravelplanner.utils.Friend;
 import com.example.pplki18.grouptravelplanner.utils.SessionManager;
 
 import java.util.ArrayList;
