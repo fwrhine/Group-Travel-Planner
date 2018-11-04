325f34e2d702554369fefad9fd5fa9e9118b1509
aghniaprawira <aghnia@staff.gramedia.com>
2018-11-04 21:23:06 +0700

    hotel dialog all onclicklisteners, sort dialog

--- app/src/main/java/com/example/pplki18/grouptravelplanner/BookHotelFragment.java
+++ app/src/main/java/com/example/pplki18/grouptravelplanner/BookHotelFragment.java
@@ -1,24 +1,40 @@
 package com.example.pplki18.grouptravelplanner;
 
+import android.app.DatePickerDialog;
 import android.app.Dialog;
+import android.content.DialogInterface;
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.Parcelable;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.constraint.ConstraintLayout;
 import android.support.v4.app.Fragment;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.widget.DefaultItemAnimator;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.SearchView;
 import android.text.TextUtils;
 import android.util.Log;
+import android.view.Gravity;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.view.Window;
+import android.view.WindowManager;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.Button;
+import android.widget.DatePicker;
 import android.widget.EditText;
 import android.widget.ImageView;
+import android.widget.LinearLayout;
+import android.widget.NumberPicker;
 import android.widget.ProgressBar;
+import android.widget.RelativeLayout;
 import android.widget.TextView;
+import android.widget.TimePicker;
 import android.widget.Toast;
 
 import com.android.volley.DefaultRetryPolicy;
@@ -28,39 +44,79 @@ import com.android.volley.Response;
 import com.android.volley.VolleyError;
 import com.android.volley.toolbox.StringRequest;
 import com.android.volley.toolbox.Volley;
+import com.example.pplki18.grouptravelplanner.utils.Event;
 import com.example.pplki18.grouptravelplanner.utils.Hotel;
 import com.example.pplki18.grouptravelplanner.utils.PaginationScrollListener;
+import com.example.pplki18.grouptravelplanner.utils.Place;
 import com.example.pplki18.grouptravelplanner.utils.RVAdapter_Hotel;
 import com.example.pplki18.grouptravelplanner.utils.SessionManager;
 
 import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
+import org.w3c.dom.Text;
 
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
+import java.util.Calendar;
+import java.util.Date;
 import java.util.List;
+import java.util.Locale;
+
+import static android.app.Activity.RESULT_OK;
 
 public class BookHotelFragment extends Fragment {
     private RecyclerView recyclerViewPlace;
     private LinearLayoutManager linearLayoutManager;
-    private ConstraintLayout infoView;
-    private ConstraintLayout closeSearchView;
-    private ImageView search;
-    private ImageView close;
-    private EditText checkIn;
-    private EditText checkOut;
-    private TextView textView;
     private RVAdapter_Hotel adapter;
-    private ProgressBar progressBar;
     private RequestQueue queue;
     private SessionManager sessionManager;
 
+    private TextView connectionText;
+    private ProgressBar progressBar;
+
+    //info view
+    private ConstraintLayout infoView;
+    private ImageView search;
+    private ImageView sort;
+    private TextView infoDate;
+    private TextView infoGuest;
+    private TextView infoRoom;
+
+    //close search view
+    private ConstraintLayout closeSearchView;
+    private ImageView close;
+
     private Dialog searchDialog;
+
+    //info dialog
+    private Dialog infoDialog;
+    private DatePickerDialog fromDatePickerDialog;
+    private DatePickerDialog toDatePickerDialog;
+
+    //date formatter
+    private SimpleDateFormat dateFormatter1;
+    private SimpleDateFormat dateFormatter2;
+    private SimpleDateFormat dateFormatter3;
 
     private String region;
     private String regionCode;
-    private String checkInDate;
-    private String checkOutDate;
+
+    private String sortBy = "popularity";
+    private int sortByInt = 0;
+    private Date checkInDate;
+    private Date checkOutDate;
+    private long nights = 1;
+    private String numOfGuest = "1";
+    private String numOfRoom = "1";
+
+    private Date checkInDateTemp;
+    private Date checkOutDateTemp;
+    private long nightsTemp = 1;
+    private String numOfGuestTemp = "1";
+    private String numOfRoomTemp = "1";
+
 
     private boolean isLoading = false;
     private boolean isLastPage = false;
@@ -76,10 +132,11 @@ public class BookHotelFragment extends Fragment {
         super.onActivityCreated(savedInstanceState);
         init();
 
-        textView.setVisibility(View.GONE);
+        connectionText.setVisibility(View.GONE);
         closeSearchView.setVisibility(View.GONE);
         progressBar.setVisibility(View.VISIBLE);
 
+        loadInfoView();
 
         recyclerViewPlace.setHasFixedSize(true);
         recyclerViewPlace.setLayoutManager(linearLayoutManager);
@@ -107,9 +164,17 @@ public class BookHotelFragment extends Fragment {
         search.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                showPopup(getView());
+                showSearchPopup(getView());
             }
         });
+
+        sort.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                showSortPopup();
+            }
+        });
+
 
         close.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -119,6 +184,13 @@ public class BookHotelFragment extends Fragment {
                 generateToken();
                 adapter.clear();
                 progressBar.setVisibility(View.VISIBLE);
+            }
+        });
+
+        infoView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                showInfoPopup(getView());
             }
         });
 
@@ -197,7 +269,15 @@ public class BookHotelFragment extends Fragment {
 
     }
 
-    public void showPopup(View v) {
+    public void loadInfoView() {
+        String date = dateFormatter3.format(checkInDate) + " - " + dateFormatter3.format(checkOutDate);
+
+        infoDate.setText(date);
+        infoGuest.setText(numOfGuest);
+        infoRoom.setText(numOfRoom);
+    }
+
+    public void showSearchPopup(View v) {
         searchDialog.setContentView(R.layout.search_dialog);
         SearchView searchView = (SearchView) searchDialog.findViewById(R.id.search_hotel);
         searchView.setOnQueryTextListener(new SearchView.OnQueryTextListener() {
@@ -223,6 +303,206 @@ public class BookHotelFragment extends Fragment {
             }
         });
         searchDialog.show();
+        searchView.requestFocus();
+    }
+
+    public void showSortPopup() {
+        AlertDialog.Builder builder = new AlertDialog.Builder(getContext());
+        builder.setTitle("Sort by");
+
+        // add a radio button list
+        String[] sort = {"Popularity", "Recommended", "Lowest price", "Distance"};
+        final String[] actualSort = {"popularity", "recommended", "priceLow", "distLow"};
+        int checkedItem = sortByInt;
+        builder.setSingleChoiceItems(sort, checkedItem, new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                sortBy = actualSort[which];
+                sortByInt = which;
+            }
+        });
+
+        builder.setPositiveButton("OK", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+
+            }
+        });
+
+// create and show the alert dialog
+        AlertDialog dialog = builder.create();
+        dialog.show();
+    }
+
+    public void showInfoPopup(View v) {
+        infoDialog.setContentView(R.layout.hotel_dialog);
+
+        // set position
+        Window window = infoDialog.getWindow();
+        WindowManager.LayoutParams wlp = window.getAttributes();
+        wlp.gravity = Gravity.TOP;
+        window.setAttributes(wlp);
+
+        // set animation
+        infoDialog.getWindow().getAttributes().windowAnimations = R.style.DialogAnimation;
+
+        // elements in infoDialog
+        final TextView dialogCheckIn = (TextView) infoDialog.findViewById(R.id.start_date);
+        final TextView dialogCheckOut = (TextView) infoDialog.findViewById(R.id.end_date);
+        final RelativeLayout checkInLayout = (RelativeLayout) infoDialog.findViewById(R.id.check_in);
+        final RelativeLayout checkOutLayout = (RelativeLayout) infoDialog.findViewById(R.id.check_out);
+        final TextView dialogGuest = (TextView) infoDialog.findViewById(R.id.guest);
+        final TextView dialogRoom = (TextView) infoDialog.findViewById(R.id.room);
+        final Button dialogSearch = (Button) infoDialog.findViewById(R.id.search_button);
+        final TextView dialogDays = (TextView) infoDialog.findViewById(R.id.days);
+
+        final Calendar newCalendar = Calendar.getInstance();
+
+        dialogGuest.setText(numOfGuest);
+        dialogRoom.setText(numOfRoom);
+        dialogCheckIn.setText(dateFormatter1.format(checkInDate));
+        dialogCheckOut.setText(dateFormatter1.format(checkOutDate));
+        dialogDays.setText(String.valueOf(nights));
+
+        checkInLayout.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                try {
+                    Calendar curr = Calendar.getInstance();
+                    curr.setTime(checkInDateTemp);
+                    newCalendar.setTime(dateFormatter1.parse(dialogCheckIn.getText().toString()));
+                    newCalendar.set(Calendar.YEAR, curr.get(Calendar.YEAR));
+                } catch (ParseException e) {
+                    e.printStackTrace();
+                }
+
+                fromDatePickerDialog = new DatePickerDialog(getContext(), new DatePickerDialog.OnDateSetListener() {
+
+                    public void onDateSet(DatePicker view, int year, int monthOfYear, int dayOfMonth) {
+                        Calendar newDate = Calendar.getInstance();
+                        newDate.set(year, monthOfYear, dayOfMonth);
+                        checkInDateTemp = newDate.getTime();
+                        dialogCheckIn.setText(dateFormatter1.format(checkInDateTemp));
+
+                        if (!dialogCheckIn.getText().toString().equals("Date") &&
+                                !dialogCheckOut.getText().toString().equals("Date")) {
+                            long diff = checkOutDateTemp.getTime() - checkInDateTemp.getTime();
+                            nightsTemp = (diff / (24 * 60 * 60 * 1000) + 2) - 1;
+                            dialogDays.setText(nightsTemp + "");
+                        }
+                    }
+                },newCalendar.get(Calendar.YEAR), newCalendar.get(Calendar.MONTH), newCalendar.get(Calendar.DAY_OF_MONTH));
+                fromDatePickerDialog.getDatePicker().setMinDate(Calendar.getInstance().getTimeInMillis());
+                fromDatePickerDialog.show();
+            }
+        });
+
+        checkOutLayout.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                try {
+                    Calendar curr = Calendar.getInstance();
+                    curr.setTime(checkOutDateTemp);
+                    newCalendar.setTime(dateFormatter1.parse(dialogCheckOut.getText().toString()));
+                    newCalendar.set(Calendar.YEAR, curr.get(Calendar.YEAR));
+                } catch (ParseException e) {
+                    e.printStackTrace();
+                }
+
+                toDatePickerDialog = new DatePickerDialog(getContext(), new DatePickerDialog.OnDateSetListener() {
+
+                    public void onDateSet(DatePicker view, int year, int monthOfYear, int dayOfMonth) {
+                        Calendar newDate = Calendar.getInstance();
+                        newDate.set(year, monthOfYear, dayOfMonth);
+                        checkOutDateTemp = newDate.getTime();
+                        dialogCheckOut.setText(dateFormatter1.format(checkOutDateTemp));
+
+                        if (!dialogCheckIn.getText().toString().equals("Date") &&
+                                !dialogCheckOut.getText().toString().equals("Date")) {
+                            long diff = checkOutDateTemp.getTime() - checkInDateTemp.getTime();
+                            nightsTemp = (diff / (24 * 60 * 60 * 1000) + 1) - 1;
+                            dialogDays.setText(nightsTemp + "");
+                        }
+                    }
+                },newCalendar.get(Calendar.YEAR), newCalendar.get(Calendar.MONTH), newCalendar.get(Calendar.DAY_OF_MONTH));
+                toDatePickerDialog.getDatePicker().setMinDate(checkInDateTemp.getTime());
+                toDatePickerDialog.show();
+
+            }
+        });
+
+        dialogGuest.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                AlertDialog.Builder builder = new AlertDialog.Builder(getActivity());
+                LayoutInflater inflater = getLayoutInflater();
+                View dialogLayout = inflater.inflate(R.layout.number_picker_dialog, null);
+                final NumberPicker numberPicker = dialogLayout.findViewById(R.id.number_picker);
+
+                numberPicker.setMinValue(1);
+                numberPicker.setMaxValue(32);
+                numberPicker.setValue(Integer.parseInt(dialogGuest.getText().toString()));
+
+                builder.setView(dialogLayout);
+                builder.setPositiveButton("OK",
+                        new DialogInterface.OnClickListener() {
+                            public void onClick(DialogInterface dialog, int which) {
+                                dialog.dismiss();
+                                numOfGuestTemp = String.valueOf(numberPicker.getValue());
+                                dialogGuest.setText(numOfGuestTemp);
+                            }
+                        });
+
+                builder.show();
+            }
+        });
+
+        dialogRoom.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                AlertDialog.Builder builder = new AlertDialog.Builder(getActivity());
+                LayoutInflater inflater = getLayoutInflater();
+                View dialogLayout = inflater.inflate(R.layout.number_picker_dialog, null);
+                final NumberPicker numberPicker = dialogLayout.findViewById(R.id.number_picker);
+
+                numberPicker.setMinValue(1);
+                numberPicker.setMaxValue(8);
+                numberPicker.setValue(Integer.parseInt(dialogRoom.getText().toString()));
+
+                builder.setView(dialogLayout);
+                builder.setPositiveButton("OK",
+                        new DialogInterface.OnClickListener() {
+                            public void onClick(DialogInterface dialog, int which) {
+                                dialog.dismiss();
+                                numOfRoomTemp = String.valueOf(numberPicker.getValue());
+                                dialogRoom.setText(numOfRoomTemp);
+                            }
+                        });
+
+                builder.show();
+            }
+        });
+
+
+
+        dialogSearch.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                numOfGuest = numOfGuestTemp;
+                numOfRoom = numOfRoomTemp;
+                checkInDate = checkInDateTemp;
+                checkOutDate = checkOutDateTemp;
+                nights = nightsTemp;
+                infoDialog.dismiss();
+                loadInfoView();
+            }
+        });
+
+        infoDialog.show();
+    }
+
+    private void numberPicker(String type) {
+
     }
 
     public void generateToken() {
@@ -400,19 +680,45 @@ public class BookHotelFragment extends Fragment {
 
     private void init() {
         recyclerViewPlace = (RecyclerView) getView().findViewById(R.id.rv);
-        infoView = (ConstraintLayout) getView().findViewById(R.id.info);
-        closeSearchView = (ConstraintLayout) getView().findViewById(R.id.close_search_view);
-        progressBar = (ProgressBar) getView().findViewById(R.id.main_progress);
-        search = (ImageView) getView().findViewById(R.id.search);
-        close = (ImageView) getView().findViewById(R.id.close);
-        searchDialog = new Dialog(getContext());
-//        checkIn = (EditText) getView().findViewById(R.id.check_in);
-//        checkOut = (EditText) getView().findViewById(R.id.check_out);
-        textView = (TextView) getView().findViewById(R.id.connection);
         linearLayoutManager = new LinearLayoutManager(getActivity());
         queue = Volley.newRequestQueue(getActivity());
         sessionManager = new SessionManager(getActivity().getApplicationContext());
         adapter = new RVAdapter_Hotel(getContext());
+
+        progressBar = (ProgressBar) getView().findViewById(R.id.main_progress);
+        connectionText = (TextView) getView().findViewById(R.id.connection);
+
+        //info
+        infoView = (ConstraintLayout) getView().findViewById(R.id.info);
+        search = (ImageView) getView().findViewById(R.id.search);
+        sort = (ImageView) getView().findViewById(R.id.sort);
+        infoDialog = new Dialog(getContext(), R.style.Theme_Dialog);
+        infoDate = (TextView) getView().findViewById(R.id.date);
+        infoGuest = (TextView) getView().findViewById(R.id.guest);
+        infoRoom = (TextView) getView().findViewById(R.id.room);
+
+        //search
+        closeSearchView = (ConstraintLayout) getView().findViewById(R.id.close_search_view);
+        close = (ImageView) getView().findViewById(R.id.close);
+        searchDialog = new Dialog(getContext());
+
+        dateFormatter1 = new SimpleDateFormat("EEE, MMM d", Locale.US);
+        dateFormatter2 = new SimpleDateFormat("d MMMM yyyy", Locale.US);
+        dateFormatter3 = new SimpleDateFormat("MMM d", Locale.US);
+        try {
+            checkInDateTemp = dateFormatter2.parse(getArguments().getString("date"));
+            checkInDate = checkInDateTemp;
+
+            Calendar c = Calendar.getInstance();
+            c.setTime(checkInDate);
+            c.add(Calendar.DATE, 1);
+
+            checkOutDateTemp = c.getTime();
+            checkOutDate = checkOutDateTemp;
+        } catch (ParseException e) {
+            e.printStackTrace();
+        }
+
         region = sessionManager.getCurrentRegion();
         regionCode = "294226";
     }
