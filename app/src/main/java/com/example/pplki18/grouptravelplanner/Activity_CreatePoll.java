7e44ad2ba308c52f60ba7f3efa086b2812fbedf6
unknown <bayusuto@gmail.com>
2018-11-10 20:17:21 +0700

    create poll activity UI

---
+++ app/src/main/java/com/example/pplki18/grouptravelplanner/Activity_CreatePoll.java
@@ -0,0 +1,58 @@
+package com.example.pplki18.grouptravelplanner;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageButton;
+import android.widget.TextView;
+
+import java.util.ArrayList;
+
+public class Activity_CreatePoll extends AppCompatActivity {
+
+    EditText topicInput;
+    EditText choiceInput;
+    TextView choiceText;
+    Button btnAddChoice;
+    ImageButton btndone;
+    ArrayList<String> choiceList;
+    String choices;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_create_poll);
+        init();
+        btnAddChoice.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                // Starts the function below
+                String newChoice = choiceText.getText().toString();
+                choiceList.add(newChoice);
+                choices += newChoice + " ";
+                choiceText.setText(choices);
+            }
+        });
+
+        btndone.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                // Starts the function below
+                Poll newPoll = new Poll(choiceList);
+            }
+        });
+    }
+
+
+    private void init() {
+        topicInput = (EditText) findViewById(R.id.poll_topicText);
+        choiceInput = (EditText) findViewById(R.id.poll_choiceText);
+        choiceText = (TextView) findViewById(R.id.poll_choicesList);
+        btnAddChoice = (Button) findViewById(R.id.btn_poll_add_choice);
+        btndone = (ImageButton) findViewById(R.id.btn_done_poll);
+        choiceList = new ArrayList<>();
+        choices = "";
+    }
+}
---
+++ app/src/main/java/com/example/pplki18/grouptravelplanner/Poll.java
@@ -0,0 +1,48 @@
+package com.example.pplki18.grouptravelplanner;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+
+public class Poll {
+
+    private ArrayList<String> voters;
+    private ArrayList<String> alreadyVoted;
+    private ArrayList<String> choiceList;
+    private HashMap<String, Integer> choiceMap;
+
+    public Poll(ArrayList<String> list){
+        this.choiceList = list;
+        this.alreadyVoted = new ArrayList<String>();
+        for (int ii = 0; ii < this.choiceList.size(); ii++){
+            choiceMap.put(choiceList.get(ii), 0);
+        }
+    }
+
+    //Getter
+
+    public ArrayList<String> getVoters() {
+        return voters;
+    }
+
+    public ArrayList<String> getAlreadyVoted() {
+        return alreadyVoted;
+    }
+
+    public ArrayList<String> getChoiceList() {
+        return choiceList;
+    }
+
+    public HashMap<String, Integer> getChoiceMapc() {
+        return choiceMap;
+    }
+
+    //Setter
+    public void setVoters(ArrayList<String> voters) {
+        this.voters = voters;
+    }
+
+    public void setAlreadyVoted(ArrayList<String> alreadyVoted) {
+        this.alreadyVoted = alreadyVoted;
+    }
+
+}
